e6a7a0e3fb031af30c496e5d11ab41ad7a672c60
AngelinaRajesh <>
2021-05-19 01:00:41 +0000

    parsing

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function toggle_visibility() {
   }
 }
 
-function toggle_visibility() {
+function toggle_visibility(id) {
   let e = document.getElementById(id)
   if(e.style.display == 'none')
     e.style.display = 'block'
